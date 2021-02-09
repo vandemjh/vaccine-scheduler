2451938708278285c58d737f983fa44faec94bc2
vandemjh <jack@jackv.dev>
2021-02-09 13:45:24 -0500

    Added wait time to avoid being banned from cvs

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const cvs = require('./src/scrape');
 var args = process.argv.slice(2);
 
+const WAIT_TIME = 1
+
 for (arg of [...args]) {
-    cvs.scrapeCovidAppointments(arg, 1).then(appt => console.log(appt))
+    cvs.scrapeCovidAppointments(arg, WAIT_TIME).then(appt => console.log(appt))
 }
